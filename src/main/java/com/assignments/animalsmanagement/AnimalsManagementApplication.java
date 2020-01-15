66b89db18f5f531f8cd24f26af65156af5c37a42
quangtiendinh <dinhquangtien0169@gmail.com>
2020-01-16 01:01:49 +0700

    create project skeleton

---
+++ src/main/java/com/assignments/animalsmanagement/AnimalsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.assignments.animalsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignments/animalsmanagement/AnimalsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignments.animalsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
