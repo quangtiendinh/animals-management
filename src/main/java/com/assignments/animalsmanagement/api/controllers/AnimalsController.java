0d501c4cc730b9318532903b3c113348494a3a98
quangtiendinh <dinhquangtien0169@gmail.com>
2020-01-16 01:27:31 +0700

    add spring-boot library

---
+++ src/main/java/com/assignments/animalsmanagement/api/controllers/AnimalsController.java
@@ -0,0 +1,8 @@
+package com.assignments.animalsmanagement.api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnimalsController {
+
+}
